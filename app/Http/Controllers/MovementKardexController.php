d055a06fd2e4843b10c432a83be4cc2559adf6df
GeyserVelasquez <gvelasquez.9312@unimar.edu.ve>
2026-06-28 19:42:39 -0400

    feat(movement-kardex): transition MovementKardex module to Spatie Query Builder

--- app/Http/Controllers/MovementKardexController.php
+++ app/Http/Controllers/MovementKardexController.php
@@ -6,18 +6,26 @@ use App\Http\Requests\MovementKardex\StoreMovementKardexRequest;
 use App\Http\Requests\MovementKardex\UpdateMovementKardexRequest;
 use App\Http\Resources\MovementKardexResource;
 use App\Models\MovementKardex;
+use App\Services\QueryBuilderService;
 use Illuminate\Http\Request;
 
 class MovementKardexController extends Controller
 {
+    public function __construct(
+        protected QueryBuilderService $queryBuilderService
+    ) {}
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $movements = MovementKardex::all()->toResourceCollection();
+        $query = $this->queryBuilderService->build(MovementKardex::class, $request);
 
-        return $movements;
+        $movements = $query->paginate($request->get('per_page', 15))
+            ->withQueryString();
+
+        return MovementKardexResource::collection($movements);
     }
 
     /**
@@ -29,15 +37,18 @@ class MovementKardexController extends Controller
 
         $movement = MovementKardex::create($data);
 
-        return (new MovementKardexResource($movement));
+        return new MovementKardexResource($movement);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(MovementKardex $movementKardex)
+    public function show(Request $request, MovementKardex $movementKardex)
     {
-        return (new MovementKardexResource($movementKardex));
+        $loadedMovement = $this->queryBuilderService->buildForModel($movementKardex, $request)
+            ->firstOrFail();
+
+        return new MovementKardexResource($loadedMovement);
     }
 
     /**
@@ -49,7 +60,7 @@ class MovementKardexController extends Controller
 
         $movementKardex->update($data);
 
-        return (new MovementKardexResource($movementKardex));
+        return new MovementKardexResource($movementKardex);
     }
 
     /**
--- app/Models/MovementKardex.php
+++ app/Models/MovementKardex.php
@@ -2,23 +2,26 @@
 
 namespace App\Models;
 
+use App\Attributes\Filterable;
 use App\Attributes\Includable;
+use App\Attributes\Sortable;
 use App\Enums\MovementType;
 use App\Observers\MovementKardexObserver;
-use App\Traits\HasInclude;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[Fillable(['item_id', 'item_type', 'type', 'quantity', 'event_id', 'event_type', 'date'])]
 #[Includable(['item', 'event'])]
+#[Filterable(['item_type', 'item_id', 'type', 'event_type', 'event_id', 'date'])]
+#[Sortable(['id', 'quantity', 'date', 'created_at'])]
 #[ObservedBy(MovementKardexObserver::class)]
 class MovementKardex extends Model
 {
-    use SoftDeletes, HasFactory, HasInclude;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'movement_kardex';
 
