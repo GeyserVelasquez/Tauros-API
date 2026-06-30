39bd4dcfb98500b72d8d2a1864f09242465983f8
Geyser Martin Velásquez Cedeño <135556841+GeyserVelasquez@users.noreply.github.com>
2026-06-30 02:59:51 -0400

    Fix: Refactor UserFactory to use app() for Faker
    Updated UserFactory to use Faker from Laravel container.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,9 +24,12 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        // Resolvemos Faker de manera segura directamente desde el contenedor de Laravel
+        $faker = app(\Faker\Generator::class);
+
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
