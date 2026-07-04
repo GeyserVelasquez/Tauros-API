5fe7196dc9921a7a1e42eaac9afbdd1deed9d1e4
GeyserVelasquez <gvelasquez.9312@unimar.edu.ve>
2026-07-04 03:13:41 -0400

    feat(livestock): Add reproductive_status at LivestockResource.php to include attribute in responses

--- app/Http/Resources/LivestockResource.php
+++ app/Http/Resources/LivestockResource.php
@@ -27,6 +27,7 @@ class LivestockResource extends JsonResource
             'is_alive' => (bool)$this->is_alive,
             'animal_category' => $this->animal_category?->value,
             'entry_cause_id' => $this->entry_cause_id,
+            'reproductive_status' => $this->reproductive_status,
             'state_id' => $this->state_id,
             'breed_id' => $this->breed_id,
             'color_id' => $this->color_id,
